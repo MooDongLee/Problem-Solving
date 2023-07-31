02f8c87fea22f028eb6f2de11df28e2c17c31c60
Dave <gulume0226@gmail.com>
2023-08-01 03:35:38 +0900

    [level 1] Title: 폰켓몬, Time: 0.31 ms, Memory: 4.23 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.cpp
@@ -0,0 +1,15 @@
+# include <vector>
+# include <set>
+using namespace std;
+
+int solution(vector<int> nums) {
+    int answer = 0;
+    set<int> s;
+    
+    for(int i = 0; i < nums.size(); i++) {
+        s.insert(nums[i]);
+    }
+    
+    if(s.size() >= nums.size() / 2) return nums.size() / 2;
+    else return s.size();
+}
