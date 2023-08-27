6fe7c9a9e91e72b256932f72388d6bbe7027d2e6
Dave <gulume0226@gmail.com>
2023-08-27 20:04:36 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <math.h>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    int m=int(sqrt(double(n)));
+    
+    if(m*m==n) answer=1;
+    else answer=2;
+    
+    return answer;
+}
